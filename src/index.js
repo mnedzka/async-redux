2a46bfeefde6387721f6ed683f309f8557a2fcae
Michal Nedzka <michael.nedzka@gmail.com>
2019-04-21 21:35:03 +0200

    Add reducers and connect the store to app and thunk to it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import '../node_modules/bulma/css/bulma.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import rootReducer from './reducers';
+import { fetchAllPosts } from './actions/index';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+store.dispatch(fetchAllPosts());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
